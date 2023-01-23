fa1cb836611060c7f8a8aa50c3e9cf206a9bb878
Mayur Patel <85622181+01mayurpatel@users.noreply.github.com>
2023-01-23 15:50:08 +0530

    Create index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require("http");
 const fs = require("fs");
 var requests = require("requests");
-const homeFile = fs.readFileSync("home.html","utf-8");
+const homeFile = fs.readFileSync("index.html","utf-8");
 const replaceVal = (tempVal,orgVal) =>{
     let temperature = tempVal.replace("{%tempval%}",(orgVal.main.temp - 273.15).toFixed(2))
     temperature = temperature.replace("{%tempmin%}",(orgVal.main.temp_min - 273.15).toFixed(2))
